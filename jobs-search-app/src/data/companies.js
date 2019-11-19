23e4e9b39e11e4a905080a4934d1d15976d70fb0
Lidii <lidiiany92@gmail.com>
2019-11-19 12:52:37 -0800

    created companies data

---
+++ jobs-search-app/src/data/companies.js
@@ -0,0 +1,71 @@
+const companies = [
+    {
+        "id": 1,
+        "company_name": "Company A",
+        "requirements": ["property insurance"],
+        "optional_requirements": [["apartment", "house"]]
+    },
+
+    {
+        "id": 2,
+        "company_name": "Company B",
+        "requirements": ["driver's license", "car insurance"],
+        "optional_requirements": [["5 door car", "4 door car"]]
+    },
+
+    {
+        "id": 3,
+        "company_name": "Company C",
+        "requirements": ["social security", "work permit"],
+        "optional_requirements": [[]]
+    },
+
+    {
+        "id": 4,
+        "company_name": "Company D",
+        "requirements": [],
+        "optional_requirements": [["apartment", "flat", "house"]]
+    },
+
+    {
+        "id": 5,
+        "company_name": "Company E",
+        "requirements": ["driver's license"],
+        "optional_requirements": [["2 door car", "3 door car", "4 door car", "5 door car"]]
+    },
+
+    {
+        "id": 6,
+        "company_name": "Company F",
+        "requirements": [],
+        "optional_requirements": [["scooter", "bike"], ["motorcycle", "driver's license", "motorcycle insurance"]]
+    },
+
+    {
+        "id": 7,
+        "company_name": "Company G",
+        "requirements": ["massage qualification certificate", "liability insurance"],
+        "optional_requirements": [[]]
+    },
+
+    {
+        "id": 8,
+        "company_name": "Company H",
+        "requirements": [],
+        "optional_requirements": [["storage place", "garage"]]
+    },
+
+    {
+        "id": 9,
+        "company_name": "Company J",
+        "requirements": [],
+        "optional_requirements": [[]]
+    },
+
+    {
+        "id": 10,
+        "company_name": "Company K",
+        "requirements": ["paypal account"],
+        "optional_requirements": [[]]
+    },
+];
